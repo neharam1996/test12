3a88858efda04a768ada022c70065c1ff281931b
sneharam <sneharam@localhost>
2020-12-09 18:54:41 -0500

    newone

---
+++ SunHomeAuthorization/src/main/java/com/tcs/sunhomeauth/SunHomeAuthorizationApplication.java
@@ -0,0 +1,13 @@
+package com.tcs.sunhomeauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunHomeAuthorizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SunHomeAuthorizationApplication.class, args);
+	}
+
+}
---
+++ SunHomeAuthorization/src/test/java/com/tcs/sunhomeauth/SunHomeAuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcs.sunhomeauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunHomeAuthorizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
